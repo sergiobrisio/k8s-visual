26890c2f3cfa2a055cc56176a72267373e766571
Stefano.BRIVIO@vodafone.com <Stefano.BRIVIO@vodafone.com>
2023-12-20 10:59:35 +0100

    changes suggested to modify hierarchy

--- script.js
+++ script.js
@@ -1,88 +1,66 @@
 document.getElementById('fileInput').addEventListener('change', function(event) {
     const reader = new FileReader();
     reader.onload = function() {
-        const allYamlDocs = reader.result.split(/\n---\s*\n/); // Improved splitting logic
-        allYamlDocs.forEach(yamlDoc => {
-            if (yamlDoc.trim()) {
-                try {
-                    const yamlData = jsyaml.load(yamlDoc);
-                    visualizeK8sObjects(yamlData);
-                } catch (e) {
-                    console.error(e);
-                    alert("Error parsing YAML document.");
-                }
-            }
-        });
+        const allYamlDocs = reader.result.split(/\n---\s*\n/);
+        const k8sObjects = allYamlDocs.map(yamlDoc => yamlDoc.trim() ? jsyaml.load(yamlDoc) : null).filter(obj => obj);
+        visualizeK8sObjects(k8sObjects);
     };
     reader.readAsText(event.target.files[0]);
 });
 
-function visualizeK8sObjects(data) {
+function visualizeK8sObjects(objects) {
     const architectureDiv = document.getElementById('architecture');
-    // Process each YAML document
-    if (data) {
-        processK8sObject(data, architectureDiv);
+    architectureDiv.innerHTML = ''; // Clear previous visualization
+
+    objects.forEach(obj => processK8sObject(obj, architectureDiv, objects));
+    // Call drawDependencies here if implementing connections
+}
+
+function processK8sObject(object, container, allObjects) {
+    if (!object || !object.kind) return;
+
+    const element = document.createElement('div');
+    element.classList.add('k8s-object');
+    element.setAttribute('data-kind', object.kind);
+    element.setAttribute('data-name', object.metadata.name);
+
+    switch (object.kind) {
+        case 'Service':
+            processService(object, element, allObjects);
+            break;
+        case 'Deployment':
+            processDeployment(object, element);
+            break;
+        // Add cases for other kinds as needed
     }
-    drawDependencies(architectureDiv);
+
+    container.appendChild(element);
 }
 
-function processK8sObject(object, container) {
-    if (object && object.kind) {
-        const kind = object.kind;
-        const name = object.metadata && object.metadata.name ? object.metadata.name : 'Unnamed';
-        const element = document.createElement('div');
-        element.classList.add('k8s-object');
-        element.setAttribute('data-kind', kind);
-        element.setAttribute('data-name', name);
-        element.textContent = `Kind: ${kind}, Name: ${name}`;
-        container.appendChild(element);
-    }
+function processService(service, element, allObjects) {
+    element.textContent = `Service: ${service.metadata.name}`;
+    // Logic to find and display related deployments
+    // This is a placeholder; you'll need to implement matching logic based on selectors, etc.
 }
 
-function drawDependencies(container) {
-    // Example logic to draw dependencies (you'll need to expand this)
-    // This is a placeholder for demonstration purposes
-    const objects = container.getElementsByClassName('k8s-object');
-    for (let i = 0; i < objects.length; i++) {
-        for (let j = 0; j < objects.length; j++) {
-            if (i !== j && shouldConnect(objects[i], objects[j])) {
-                // Draw an arrow or line between objects[i] and objects[j]
-                // Implement your own logic for drawing connections
-                console.log(`Draw a connection from ${objects[i].getAttribute('data-name')} to ${objects[j].getAttribute('data-name')}`);
-            }
-        }
-    }
-}
-
-function shouldConnect(obj1, obj2) {
-    // Placeholder logic to determine if there should be a connection
-    // Implement your own logic based on Kubernetes relationships
-    return Math.random() > 0.5; // Randomly decides to connect for demonstration
-}
-
-// Add CSS for visualization (e.g., positioning of elements, arrows)
-// ... [Previous JavaScript code] ...
-
-function drawDependencies(container) {
-    const objects = container.getElementsByClassName('k8s-object');
-    let yPos = 20;
-
-    for (let i = 0; i < objects.length; i++) {
-        objects[i].style.top = `${yPos}px`;
-        objects[i].style.left = `${i * 150}px`;
-        yPos += 60;
-
-        for (let j = 0; j < objects.length; j++) {
-            if (i !== j && shouldConnect(objects[i], objects[j])) {
-                const line = document.createElement('div');
-                line.className = 'connection-line';
-                line.style.top = `${Math.min(parseInt(objects[i].style.top), parseInt(objects[j].style.top)) + 25}px`;
-                line.style.left = `${Math.min(parseInt(objects[i].style.left), parseInt(objects[j].style.left)) + 75}px`;
-                line.style.width = `${Math.abs(parseInt(objects[i].style.left) - parseInt(objects[j].style.left))}px`;
-                container.appendChild(line);
-            }
-        }
+function processDeployment(deployment, element) {
+    element.textContent = `Deployment: ${deployment.metadata.name}`;
+    const replicaCount = deployment.spec.replicas || 1;
+    for (let i = 0; i < replicaCount; i++) {
+        const pod = document.createElement('div');
+        pod.classList.add('k8s-pod');
+        pod.textContent = `Pod ${i + 1} of ${deployment.metadata.name}`;
+        // Display container names and images
+        const containers = deployment.spec.template.spec.containers;
+        containers.forEach(container => {
+            const containerDiv = document.createElement('div');
+            containerDiv.classList.add('k8s-container');
+            containerDiv.textContent = `Container: ${container.name}, Image: ${container.image}`;
+            pod.appendChild(containerDiv);
+        });
+        element.appendChild(pod);
     }
 }
 
-// ... [Rest of the JavaScript code] ...
+// ... Add more functions or logic as needed ...
+// ... Add CSS for visualization ...
