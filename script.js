aadfae415bbb87fa2b9ec3ff108aac7c2753747f
Stefano.BRIVIO@vodafone.com <Stefano.BRIVIO@vodafone.com>
2023-12-20 10:22:08 +0100

    change of js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
     reader.onload = function() {
         try {
             const yamlData = jsyaml.load(reader.result);
-            visualizeArchitecture(yamlData);
+            visualizeK8sObjects(yamlData);
         } catch (e) {
             console.error(e);
             alert("Error parsing YAML file.");
@@ -12,13 +12,26 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
     reader.readAsText(event.target.files[0]);
 });
 
-function visualizeArchitecture(data) {
+function visualizeK8sObjects(data) {
     const architectureDiv = document.getElementById('architecture');
     architectureDiv.innerHTML = ''; // Clear previous visualization
-    // Simple visualization logic (you'll need to expand this)
-    for (const key in data) {
+
+    if (Array.isArray(data)) {
+        // Handle multiple documents in one YAML file
+        data.forEach(doc => processK8sObject(doc, architectureDiv));
+    } else {
+        // Handle a single document
+        processK8sObject(data, architectureDiv);
+    }
+}
+
+function processK8sObject(object, container) {
+    if (object && object.kind) {
+        const kind = object.kind;
+        const name = object.metadata && object.metadata.name ? object.metadata.name : 'Unnamed';
         const element = document.createElement('div');
-        element.textContent = `${key}: ${JSON.stringify(data[key])}`;
-        architectureDiv.appendChild(element);
+        element.textContent = `Kind: ${kind}, Name: ${name}`;
+        container.appendChild(element);
+        // Add more details or styling as needed
     }
 }
